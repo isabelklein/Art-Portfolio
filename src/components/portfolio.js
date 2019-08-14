0165140a017e66f62dfffa762cd19fa8e604c5eb
Isabel Klein <isabelklein@Isabels-MacBook-Pro-2.local>
2019-08-13 20:49:37 -0700

    Fix aspect ratios

--- src/components/portfolio.js
+++ src/components/portfolio.js
@@ -1,29 +1,29 @@
 import React from 'react';
-import Gallery from 'react-photo-gallery';
-
-const photos = [
-  { src: '/portfolio/0.jpg', width: 3, height: 4 },
-  { src: '/portfolio/1.jpg', width: 3, height: 4 },
-  { src: '/portfolio/2.jpg', width: 4, height: 4 },
-  { src: '/portfolio/3.jpg', width: 4, height: 4 },
-  { src: '/portfolio/4.jpg', width: 4, height: 3 },
-  { src: '/portfolio/5.jpg', width: 1, height: 1 },
-  { src: '/portfolio/6.jpg', width: 4, height: 3 },
-  { src: '/portfolio/7.jpg', width: 1, height: 1 },
-  { src: '/portfolio/8.jpg', width: 5, height: 4 },
-  { src: '/portfolio/9.jpg', width: 4, height: 3 },
-  { src: '/portfolio/10.jpg', width: 2, height: 1 },
-  { src: '/portfolio/11.jpg', width: 3, height: 2 },
-]
 
 export default class Portfolio extends React.Component {
-    render() {
-      return (
-        <div className="portfolio">
-          <Gallery photos={photos} direction="row"/>
+  render() {
+    return (
+      <div class="row">
+        <div class="column">
+          <img src="/portfolio/0.jpg" alt="" ></img>
+          <img src="/portfolio/1.jpg" alt="" ></img>
+          <img src="/portfolio/2.jpg" alt="" ></img>
+          <img src="/portfolio/3.jpg" alt="" ></img>
         </div>
-      )
-    }
+        <div class="column">
+          <img src="/portfolio/4.jpg" alt=""></img>
+          <img src="/portfolio/5.jpg" alt=""></img>
+          <img src="/portfolio/6.jpg" alt=""></img>
+          <img src="/portfolio/7.jpg" alt="" ></img>
+        </div>
+        <div class="column">
+          <img src="/portfolio/8.jpg" alt=""></img>
+          <img src="/portfolio/9.jpg" alt=""></img>
+          <img src="/portfolio/10.jpg" alt="" ></img>
+          <img src="/portfolio/11.jpg" alt=""></img>
+          <img src="/portfolio/12.jpg" alt="" ></img>
+        </div>
+      </div>
+    )
   }
-
-  
+}
