c642b248a4f29a0a6b9b25fde001781f5815c9e8
Isabel Klein <isabelklein@Isabels-MacBook-Pro-2.local>
2019-08-18 17:43:00 -0700

    Add page transitions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React from 'react';
 import './App.css';
-import Portfolio from './components/portfolio';
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-import About from './components/about';
-import Contact from './components/contact';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import Container from './components/container';
 
 class App extends React.Component {
   state = {
@@ -13,23 +11,19 @@ class App extends React.Component {
   render() {
     return (
       <Router>
-      <div className="App">
-        <div className="top-border"></div>
-        <header className="App-header">Isabel Klein
+        <div className="App">
+          <div className="top-border"></div>
+          <header className="App-header">Isabel Klein
           <div className="description">
-            artist and illustrator 
-            <Link to ="/work"> <button className="navigation" style={{top:"150px"}}>WORK</button></Link>
-            <Link to ="/about"> <button className="navigation" style= {{top:"180px"}}>ABOUT</button></Link>
-            <Link to ="/contact"> <button className="navigation" style={{top:"210px"}}>CONTACT</button></Link>
-          </div>
-        </header>
-      
-      <Switch>
-      <Route path="/work" exact component={Portfolio} />
-      <Route path="/about" exact component={About} />
-      <Route path="/contact" exact component={Contact} />
-      </Switch>
-      </div>
+              artist and illustrator
+            <Link to="/work"> <button className="navigation" style={{ top: "150px" }}>WORK</button></Link>
+              <Link to="/about"> <button className="navigation" style={{ top: "180px" }}>ABOUT</button></Link>
+              <Link to="/contact"> <button className="navigation" style={{ top: "210px" }}>CONTACT</button></Link>
+            </div>
+          </header>
+          <div className="Portfolio"></div>
+          <Container />
+        </div>
       </Router>
     );
   }
--- src/components/contact.js
+++ src/components/contact.js
@@ -24,7 +24,6 @@ export default class About extends React.Component {
                         </a>
                 </div>
             </div>
-            // </div>
         )
     }
 }
